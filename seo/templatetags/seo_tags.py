f1224b882f6e13058d7c30da00c2d6f5c0e77da6
azat.zakirov <azat.zakirov@redsolution.ru>
2018-09-25 15:10:31 +0500

    Update for template tags, added postfix and default value

--- seo/templatetags/seo_tags.py
+++ seo/templatetags/seo_tags.py
@@ -8,126 +8,157 @@ from django.utils.html import escape
 from django.core.cache import cache
 from seo.models import Seo, Url, SeoTemplate
 
-INTENTS = ['title', 'keywords', 'description', ]
-
 register = template.Library()
 
-class SeoNode(template.Node):
-    def __init__(self, intent, object, variable):
-        self.intent = intent
-        self.object = object
-        self.variable = variable
-
-    def _process_var_argument(self, context, seo):
-        if self.variable is None:
-            return escape(getattr(seo, self.intent, u''))
-        else:
-            context[self.variable] = getattr(seo, self.intent, None)
-            return u''
-
-    def _seo_by_url(self, context):
-        try:
-            request = context['request']
-        except KeyError:
-            warnings.warn('`request` was not found in context. Add "django.core.context_processors.request" to `TEMPLATE_CONTEXT_PROCESSORS` in your settings.py.')
-            return self._process_var_argument(context, None)
-        else:
-            try:
-                object = Url.objects.get(url=request.path_info)
-            except Url.DoesNotExist:
-                return self._process_var_argument(context, None)
-            else:
-                try:
-                    seo = Seo.objects.get(
-                        content_type=ContentType.objects.get_for_model(
-                                object.__class__),
-                        object_id=object.id)
-                except Seo.DoesNotExist:
-                    return self._process_var_argument(context, None)
-                else:
-                    return self._process_var_argument(context, seo)
-
-    def _seo_by_content_object(self, context):
-        object = template.Variable(self.object).resolve(context)
-        if not isinstance(object, Model):
-            return self._process_var_argument(context, None)
-
-        try:
-            seo = Seo.objects.get(
-                content_type=ContentType.objects.get_for_model(
-                        object.__class__),
-                object_id=object.id)
-        except Seo.DoesNotExist:
-            return self._seo_by_url(context)
-        else:
-            return self._process_var_argument(context, seo)
-
-    def render(self, context):
-        if self.object is None:
-            # search by URL
-            return self._seo_by_url(context)
-        else:
-            # Try to retrieve object from context
-            return self._seo_by_content_object(context)
+DEFAULT_SEO_OBJ = {
+    "title": "",
+    "desc": "",
+    "keys": ""
+}
 
 
-def seo_tag(parser, token):
-    """Get seo data for object"""
-    splited = token.split_contents()
-    if splited[1] in INTENTS:
-        if len(splited) in [4, 6] and splited[2] == 'for':
-            if len(splited) == 4:
-                return SeoNode(splited[1], splited[3], None)
-            elif splited[4] == 'as':
-                return SeoNode(splited[1], splited[3], splited[5])
-        elif len(splited) in [2, 4]:
-            if len(splited) == 2:
-                return SeoNode(splited[1], None, None)
-            elif splited[2] == 'as':
-                return SeoNode(splited[1], None, splited[3])
-    raise template.TemplateSyntaxError, "Invalid syntax. Use ``{% seo <title|keywords|description> [for <object>] [as <variable>] %}``"
-register.tag('seo', seo_tag)
+def seo_by_url(context):
+
+    """ Возвращает seo-запись по url запроса (если запись привязана к url) """
+
+    path = context['request'].path_info
+    url = Url.objects.filter(url=path).first()
+    if url:
+        url_ct = ContentType.objects.get_for_model(url.__class__)
+        seo = Seo.objects.filter(content_type=url_ct, object_id=url_ct).first()
+        return {
+            "title": seo.title,
+            "desc": seo.description,
+            "keys": seo.keywords,
+        }
+    else:
+        return DEFAULT_SEO_OBJ
+
+
+def seo_by_content_object(item, item_ct):
+
+    """ Возвращает seo-запись """
+
+    seo = Seo.objects.filter(content_type=item_ct, object_id=item.id).first()
+    if seo:
+        return {
+            "title": seo.title,
+            "desc": seo.description,
+            "keys": seo.keywords,
+        }
+    else:
+        return DEFAULT_SEO_OBJ
 
 
 def get_metatag_data(metatag_name, item_data):
-    metatag_data = item_data[metatag_name]["seo_text"]
-    if not metatag_data:
-        metatag_data = item_data[metatag_name]["gen_text"]
-    if not metatag_data:
-        metatag_data = ''
-    return metatag_data
+
+    """ Возвращает значение seo-метатега для обьекта согласно приоритету:
+        1. seo-атрибут заполненный вручную
+        2. seogen-атрибут заполненный автоматически
+        3. пустая строка
+    """
+
+    seo_text = item_data[metatag_name]["seo_text"]
+    gen_text = item_data[metatag_name]["gen_text"]
+    result = ''
+    if seo_text:
+        result = seo_text
+    elif gen_text:
+        result = gen_text
+    return result
 
 
-@register.inclusion_tag('seo/seo.html')
-def seogen(item):
+def seo_by_template(context, item, item_ct):
+
+    """ Возврашает seo-запись элемента по приоритету:
+        1. Seo-запись из шаблона родителя (если есть родитель с seo-шаблоном)
+        2. Seo-запиcь элемента
+        3. Seo-запись по url (если элемент не указан)
+        4. Пустая seo-запись
+    """
+
+    try:
+        parent_tree = item.tree.get().parent
+    except:
+        parent_tree = False
+    # есть родитель
+    if parent_tree:
+        parent = parent_tree.content_object
+        parent_ct = ContentType.objects.get_for_model(parent)
+        seo_template = SeoTemplate.objects.filter(content_type=parent_ct, object_id=parent.id).first()
+        # у родителя есть seo-шаблон
+        if seo_template:
+            item_key = "%d-%d" % (item_ct.id, item.id)
+            item_data = seo_template.data["items"][item_key]
+            # seo-запись из шаблона
+            item_seo = {
+                "title": get_metatag_data("title", item_data),
+                "desc": get_metatag_data("desc", item_data),
+                "keys": get_metatag_data("keys", item_data)
+            }
+
+    # seo запись элемента
+    elif item:
+        item_seo = seo_by_content_object(item, item_ct)
+
+    return item_seo
+
+
+def modify_seo(seo, **kwargs):
+
+    """ Добавить префикс или установить дефолтное значение метатегам """
+
+    if not seo["title"]:
+        seo["title"] = kwargs.get("title_default", '')
+    if seo["title"]:
+        title_postfix = kwargs.get("title_postfix")
+        if title_postfix:
+            seo["title"] += ' ' + title_postfix
+
+    if not seo["desc"]:
+        seo["desc"] = kwargs.get("desc_default", '')
+    if seo["desc"]:
+        desc_postfix = kwargs.get("desc_postfix")
+        if desc_postfix:
+            seo["desc"] += ' ' + desc_postfix
+
+    if not seo["keys"]:
+        seo["keys"] = kwargs.get("keys_default", '')
+    if seo["keys"]:
+        keys_postfix = kwargs.get("keys_postfix")
+        if keys_postfix:
+            seo["keys"] += ' ' + keys_postfix
+    return seo
+
+
+@register.inclusion_tag('seo/seo.html', takes_context=True)
+def seo(context, item=None, **kwargs):
     item_ct = ContentType.objects.get_for_model(item)
     item_cache_key = "seo-%d-%d" % (item_ct.id, item.id)
     item_json_seo = cache.get(item_cache_key)
     if item_json_seo:
         item_seo = json.loads(item_json_seo)
     else:
-        parent_tree = item.tree.get().parent
-        if parent_tree:
-            parent = parent_tree.content_object
-            parent_ct = ContentType.objects.get_for_model(parent)
-            seo_template = SeoTemplate.objects.filter(content_type=parent_ct, object_id=parent.id).first()
-            if seo_template:
-                item_key = "%d-%d" % (item_ct.id, item.id)
-                item_data = seo_template.data["items"][item_key]
+        if item:
+            item_seo = seo_by_content_object(item, item_ct)
+        else:
+            item_seo = seo_by_url(context)
+        item_json_seo = json.dumps(item_seo, ensure_ascii=False)
+        cache.set(item_cache_key, item_json_seo)
 
-                item_seo = {
-                    "title": get_metatag_data("title", item_data),
-                    "desc": get_metatag_data("desc", item_data),
-                    "keys": get_metatag_data("keys", item_data)
-                }
-            else:
-                item_seo = {
-                    "title": "",
-                    "desc": "",
-                    "keys": ""
-                }
+    return modify_seo(item_seo, **kwargs)
 
-            item_json_seo = json.dumps(item_seo, ensure_ascii=False)
-            cache.set(item_cache_key, item_json_seo)
 
-    return item_seo
+@register.inclusion_tag('seo/seo.html', takes_context=True)
+def seogen(context, item, **kwargs):
+
+    item_ct = ContentType.objects.get_for_model(item)
+    item_cache_key = "seo-%d-%d" % (item_ct.id, item.id)
+    item_json_seo = cache.get(item_cache_key)
+    if item_json_seo:
+        item_seo = json.loads(item_json_seo)
+    else:
+        item_seo = seo_by_template(context, item, item_ct)
+        item_json_seo = json.dumps(item_seo, ensure_ascii=False)
+        cache.set(item_cache_key, item_json_seo)
+    return modify_seo(item_seo, **kwargs)
